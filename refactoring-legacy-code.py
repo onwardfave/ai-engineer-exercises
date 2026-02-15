b2e6ac50275b503b71411d3dd9f514f8adf3177d
Sam Wambua <wambua.sam@gmail.com>
2026-02-16 00:07:42 +0300

    first commit

---
+++ refactoring-legacy-code.py
@@ -0,0 +1,10 @@
+# Legacy code to refactor
+def calc(a,b,c):
+    if c=="+":
+        return a+b
+    elif c=="-":
+        return a-b
+    elif c=="*":
+        return a*b
+    else:
+        return a/b
